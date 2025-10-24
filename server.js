5c698b9794f62939df791c969d0fe98e7468a6c4
Navanit vishwakarma <navanitvishwakarma790@gmail.com>
2025-10-24 22:17:58 +0530

    new chnages

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 
 // Routes
